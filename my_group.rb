49390cc0c24bd774524b0e5a72a9c92b3a3df9a9
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-10-10 21:39:11 +0200

    more exercise files

--- my_group.rb
+++ my_group.rb
@@ -2,6 +2,11 @@ my_group = Array.new
 
 my_group = ["person_1", "person_2", "person_3"]
 
-person = {name: "person_1", gender: "female", age: 36}
-person = {name: "person_2", gender: "male", age: 30}
-person = {name: "person_3", gender: "heshe", age: 43}
+person_1 = {name: "Anna", gender: "female", age: 38}
+person_2 = {name: "Thomas", gender: "male", age: 44}
+person_3 = {name: "David", gender: "male", age: 43}
+
+
+puts person_2[:name] + ' is a ' + person_2[:age].to_s + ' year old ' + person_2[:gender] + ''
+puts person_1[:name] + ' is a ' + person_1[:age].to_s + ' year old ' + person_1[:gender] + ''
+puts person_3[:name] + ' is a ' + person_3[:age].to_s + ' year old ' + person_3[:gender] + ''
