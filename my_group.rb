f3887325a49faab8ca3892fe59b6c0400be8b373
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-10-10 08:06:24 +0200

    initial commit

---
+++ my_group.rb
@@ -0,0 +1,7 @@
+my_group = Array.new
+
+my_group = ["person_1", "person_2", "person_3"]
+
+person = {name: "person_1", gender: "female", age: 36}
+person = {name: "person_2", gender: "male", age: 30}
+person = {name: "person_3", gender: "heshe", age: 43}
